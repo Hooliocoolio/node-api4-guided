b262947847313cf3d5bca3984c40993caabe6bf6
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-08-12 18:31:39 -0700

    change

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -4,10 +4,7 @@ const router = express.Router()
 
 router.get("/", (req, res) => {
 	res.status(200).json({
-		Welcome: `${process.env.COHORT}`, 
-	})
-	res.status(200).json({
-		FunFact: `${process.env.FUN_FACT}`, 
+		WelCome: `${process.env.FUN_FACT}`, 
 	})
 })
 
